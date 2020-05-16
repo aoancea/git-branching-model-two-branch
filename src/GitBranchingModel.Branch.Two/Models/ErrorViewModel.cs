a23054073fb3e5dc50db3a6fc220301625385444
Alexandru Oancea <alx.oancea@gmail.com>
2020-05-16 11:00:55 +0300

    initial folder structure

---
+++ src/GitBranchingModel.Branch.Two/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitBranchingModel.Branch.Two.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
